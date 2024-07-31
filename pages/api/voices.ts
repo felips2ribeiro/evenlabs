3540152f8c976cd5b63d0e9e31aece61f1c46cf6
Felipe Ribeiro <felips2ribeiro@gmail.com>
2024-07-31 15:34:35 -0300

    Enviando pra master

--- pages/api/voices.ts
+++ pages/api/voices.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const response = await fetch('https://api.elevenlabs.io/v1/voices', {
-
   });
 
   const data = await response.json();
